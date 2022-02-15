8448b7272799908ae1ea39935a750d56d405b150
Lateef Ojulari <lateef.ojulari@tcdng.com>
2022-02-15 14:58:01 +0100

    Update copyright

--- flowcentral-application/src/main/java/com/flowcentraltech/flowcentral/application/web/widgets/Filter.java
+++ flowcentral-application/src/main/java/com/flowcentraltech/flowcentral/application/web/widgets/Filter.java
@@ -1,9 +1,17 @@
 /*
-
- * Copyright (c) 2019, 2021, FlowCentral Technologies.
- * All rights reserved.
+ * Copyright 2021-2022 FlowCentral Technologies Limited.
  * 
- * PROPRIETARY AND CONFIDENTIAL. USE IS SUBJECT TO LICENSE TERMS.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.flowcentraltech.flowcentral.application.web.widgets;
 
@@ -34,7 +42,7 @@ import com.tcdng.unify.core.util.DataUtils;
 /**
  * Filter object.
  * 
- * @author Lateef Ojulari
+ * @author FlowCentral Technologies Limited
  * @since 1.0
  */
 public class Filter {
